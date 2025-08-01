a368f042ce8802e72ea3058d0c6c1a805f3cfdc4
kimyj <yongjjun8718@gmail.com>
2025-08-02 01:32:40 +0900

    init

---
+++ src/main/java/com/kyj/fmk/CntApplication.java
@@ -0,0 +1,13 @@
+package com.kyj.fmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CntApplication.class, args);
+	}
+
+}
